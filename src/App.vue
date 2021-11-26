59ec0a89caa678ff929debd3a8ad843292c4b309
hefeixiong <1074535830@qq.com>
2021-11-26 18:09:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component({
+  name: 'App',
+})
+export default class extends Vue {}
+</script>
 
 <style lang="scss">
 #app {
