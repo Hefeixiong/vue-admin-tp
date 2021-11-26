2a9ecc76e55ec81e2e65550a39935216187a4e31
hefeixiong <1074535830@qq.com>
2021-11-27 01:03:06 +0800

    初始化Sidebar

---
+++ src/layout/Sidebar/SidebarItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>xxx</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class SidebarItem extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
